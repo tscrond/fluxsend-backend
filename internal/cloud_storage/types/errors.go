cce95ea8bd481d09854e43e955a29adcb6f41c24
tscrond <tomek.skrond@gmail.com>
2026-03-26 09:45:17 +0100

    error types

--- internal/cloud_storage/types/errors.go
+++ internal/cloud_storage/types/errors.go
@@ -3,7 +3,7 @@ package types
 import "errors"
 
 var (
-	ErrFileAlreadyExists = errors.New("file already exists")
+	ErrFileAlreadyExists  = errors.New("file already exists")
 	ErrStorageUnavailable = errors.New("storage unavailable")
-	ErrUploadFailed = errors.New("upload failed")
+	ErrUploadFailed       = errors.New("upload failed")
 )
